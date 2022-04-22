38fc06b6cd5c589b7b40386a49fe568d4d7950b5
Dawid Wesolowski <wesolowskid4@gmail.com>
2022-04-22 16:32:19 +0200

    indicator for air condition bar

--- src/pages/week.tsx
+++ src/pages/week.tsx
@@ -1,14 +1,18 @@
 import type { NextPage } from 'next'
 import Image from 'next/image'
-
+import map from '../helpers/map'
 import { WEEK, AIR_CONDITION } from '../mock/weather'
+
+const MIN_AIR_CONDITION = 0;
+const MAX_AIR_CONDITION = 600;
+const CURRENT_AIR_CONDITION = 50;
 
 const Example: NextPage = () => {
   return (
-    <main className="p-5 flex flex-col h-screen space-y-5 dark:bg-neutral-900">
+    <main className="p-5 flex flex-col min-h-screen space-y-5 dark:bg-neutral-900">
       <section className='mb-auto'>
         <header className="font-semibold text-2xl mb-5 capitalize dark:text-white">This week</header>
-        <ul className='space-y-4'>
+        <ul className='space-y-7'>
           {WEEK.map(({ day, maxTemp, minTemp, icon, label }) => (
             <li key={day} className="flex items-cetner justify-between">
               <div className='flex items-center w-3/12'>
@@ -36,7 +40,13 @@ const Example: NextPage = () => {
           </div>
         </div>
         <div>
-          <div className='flex my-2'>
+          <div className='flex my-2 relative'>
+            <span
+              className='w-4 h-4 top-8 bg-blue-500 absolute rounded-full'
+              style={{
+                left: `${map(CURRENT_AIR_CONDITION, MIN_AIR_CONDITION, MAX_AIR_CONDITION, -0.5, 99.5)}%`
+              }}
+            />
             {AIR_CONDITION.map(({ color, index, size }) => (
               <div key={index} className={`flex flex-col my-2 w-${size}/12`}>
                 <span className='text-sm text-slate-400 dark:text-slate-300 pb-2'>{index}</span>
