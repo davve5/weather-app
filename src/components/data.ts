1d5b43a61c602cfc1877f48734d8a110d9ebf0a0
Dawid Wesołowski <wesolowskid4@gmail.com>
2022-04-22 10:37:05 +0200

    Create data.ts

---
+++ src/components/data.ts
@@ -0,0 +1,37 @@
+const data = [
+  {
+    value: 26,
+    hour: "4 AM",
+    nimTemperature: "18°",
+    maxTemperature: "26°",
+    icon: "icon"
+  },
+  {
+    value: 28,
+    hour: "10 PM",
+    nimTemperature: "18°",
+    maxTemperature: "28°",
+    icon: "icon"
+  },
+  {
+    value: 28,
+    hour: "1 AM",
+    nimTemperature: "18°",
+    maxTemperature: "28°",
+    icon: "icon"
+  },
+  {
+    value: 24,
+    hour: "4 AM",
+    nimTemperature: "17°",
+    maxTemperature: "24°",
+    icon: "icon"
+  },
+  {
+    value: 24,
+    hour: "7 AM",
+    nimTemperature: "17°",
+    maxTemperature: "24°",
+    icon: "icon"
+  }
+];
